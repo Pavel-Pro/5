0e4a09b6868672180b6b0dc3c8f6d920995084c1
Pavel-Pro <proskpavel@gmail.com>
2019-02-09 16:52:55 +0200

    1

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	$('#menu__icon').on('click', function() {
+		$('.menu').slideToggle(300, function() {
+			if($(this).css('display') === 'none') {
+				$(this).removeAttr('style');
+			}
+		});
+	});
+});
